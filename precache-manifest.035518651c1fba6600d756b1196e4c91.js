71d08f6870e379c54e6624c965aa1a400078a62a
Atala <atala.rchidi@gmail.com>
2020-09-06 15:28:14 -0400

    Updates

---
+++ precache-manifest.035518651c1fba6600d756b1196e4c91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b55b201ae71a6599625018571b1d0f61",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a31ad3e53222adf651d5",
+    "url": "/portfolio/static/css/main.dd6cf6a2.chunk.css"
+  },
+  {
+    "revision": "fb868b9fbe97ed580cf3",
+    "url": "/portfolio/static/js/2.f40d86be.chunk.js"
+  },
+  {
+    "revision": "d0d6b5033fd77eb6038046252d67d3b7",
+    "url": "/portfolio/static/js/2.f40d86be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a31ad3e53222adf651d5",
+    "url": "/portfolio/static/js/main.acca7f13.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "c50a1e70af1fa5444a80466230e66573",
+    "url": "/portfolio/static/media/ConUHackII.c50a1e70.png"
+  }
+]);
