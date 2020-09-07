f26d4b0327b5b0e58c40c12bd03c36c72a67aaa5
Atala <atala.rchidi@gmail.com>
2020-09-07 10:48:05 -0400

    Updates

--- precache-manifest.ceb85870a63d0012198079a1cedbdea7.js
+++ precache-manifest.ceb85870a63d0012198079a1cedbdea7.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e83aaf66c14945b1d1428e09597b2cc3",
+    "revision": "b55d4179ea7022adaddcf4f037e195bb",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "1532ad7be4b864cbf390",
+    "revision": "961ba38028b5a58a583f",
     "url": "/portfolio/static/css/main.740da029.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.1838d124.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1532ad7be4b864cbf390",
-    "url": "/portfolio/static/js/main.3d3434c9.chunk.js"
+    "revision": "961ba38028b5a58a583f",
+    "url": "/portfolio/static/js/main.25c41684.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
