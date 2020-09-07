ec684a73aa91159b3a52000ca5d460bcbe0a9591
Atala <atala.rchidi@gmail.com>
2020-09-07 10:08:44 -0400

    fixed refreshing 404 error page with HashRouter instead of BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 
 import Contact from './pages/Contact';
 import Home from './pages/Home';
@@ -10,15 +10,15 @@ import Project from './pages/Project';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter basename='/'>
       <Switch>
-        <Route exact path="/portfolio/" component={Home} />
-        <Route exact path="/portfolio/skills" component={Skill} />
-        <Route exact path="/portfolio/projects" component={Project} />
-        <Route exact path="/portfolio/contact" component={Contact} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/skills" component={Skill} />
+        <Route exact path="/projects" component={Project} />
+        <Route exact path="/contact" component={Contact} />
         <Route component={NotFound} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
