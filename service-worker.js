dfe7b293c3dd178bb16959b47ef507640d7a6833
Atala <atala.rchidi@gmail.com>
2020-09-10 18:29:08 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a423e861ca0c8abd8408316764b7863d.js"
+  "/portfolio/precache-manifest.4eac04e7c400d57b93056dfa3e5edaae.js"
 );
 
 self.addEventListener('message', (event) => {
