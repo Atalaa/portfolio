59153fb26e25fa2667556b096ed1f5f91199b1a8
Atala <atala.rchidi@gmail.com>
2020-09-26 22:39:41 -0400

    changed css with clipth-path

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,8 +11,7 @@ const Home = () => {
             <div className="homeContent">
                 <div className="content">
                     <h1>Aata Rchidi</h1>
-                    {/* u2728 */}
-                    <h2>
+                    {/* <h2>
                         <Typed 
                             strings={["Frontend developer", "Web developer", "Video gamer"]}
                             typeSpeed={68}
@@ -22,13 +21,31 @@ const Home = () => {
                             loop
                             smartBackspace
                         />
-                    </h2>
+                    </h2> */}
 
-                    <h3>
+                    {/* <h3>
                         Fluent in French and English, I'm a passionate web developer with 2 years + of experience building and maintaining websites. I have knowledge of vanilla Javascript, React, Sass.
-                    </h3>
-                    <div className="resume">
-                        <a href={resume} target="_blank" rel="noopener noreferrer">Download resume</a>
+                    </h3> */}
+
+
+                    <div className="infoHomePage">
+                        <div className="resume">
+                            <a href={resume} target="_blank" rel="noopener noreferrer">Resume</a>
+                        </div>
+                        <h2>
+                            <Typed 
+                                strings={["Frontend developer", "Web developer", "Also a Video gamer !"]}
+                                typeSpeed={68}
+                                backSpeed={10}
+                                backDelay={300}
+                                startDelay={100}
+                                loop
+                                smartBackspace
+                            />
+                        </h2>
+                        <h3>
+                            Fluent in French and English, I'm a passionate web developer with 2 years + of experience building and maintaining websites. I speak Javascript, React, Sass.
+                        </h3>
                     </div>
                 </div>
             </div>
