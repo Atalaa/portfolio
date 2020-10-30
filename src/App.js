f65cb45c1b135074546dea0a38e8ddb3dc4e28aa
Atala <atala.rchidi@gmail.com>
2020-10-29 22:12:24 -0400

    deleted files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
-
 import Contact from './pages/Contact';
 import Home from './pages/Home';
 import Skill from './pages/Skill';
 import NotFound from './pages/NotFound';
 import Project from './pages/Project';
-
-
 
 
 const App = () => {
